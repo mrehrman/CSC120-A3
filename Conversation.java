be759b95d1469c4cca7311963e354ab788f98d1f
mrehrman <mrehrman@smith.edu>
2024-09-24 22:47:01 -0400

    complete

--- Conversation.java
+++ Conversation.java
@@ -1,41 +1,56 @@
 //import scanner class to get user input
 import java.util.Scanner;
 
+
+/**
+ * The Conversation class creates a chatbot that interacts with user input.
+ * @author Maddie Rehrman
+ * @version 9/24/24
+ */
 class Conversation {
 
-  //attributes of class conversation
+  //attribute
   static String[] chatbotResponses = new String[] {"Hmmm.", "That's interesting.", "Mm-hmmm.", "Tell me more.", "Huh.", 
     "I'd like to hear more about that.", "You have a lot to say.", "Hmmm.", "Uh-huh.", "Wow."};
 
-  //methods 
-  public String isMirror(String response){
+   
+  /**
+   * Takes in a String and returns a new String with specific words mirrored.
+   * @param response The String to modify
+   * @return The modified String with words mirrored
+   */
+  public String mirrorWords(String response){
     String[] words = response.split(" ");
     int length = words.length;
-    String[] mirroredResponse = new String[length];
+    String[] mirroredResponse = new String[length]; //create a new array to store the response with mirrored words
     for (int i = 0; i < length; i++){
       String word = words[i];
-      if (word.equals("I")){
-        word = word.replace("I", "you");
+      if (word.equalsIgnoreCase("I")){
+        word = "you";
         mirroredResponse[i] = word;
       }
-      else if (word.equals("me")){
-        word = word.replace("me", "you");
+      else if (word.equalsIgnoreCase("me")){
+        word = "you";
         mirroredResponse[i] = word;
       }
-      else if (word.equals("am")){
-        word = word.replace("am", "are");
+      else if (word.equalsIgnoreCase("am")){
+        word = "are";
         mirroredResponse[i] = word;
       }
-      else if (word.equals("you")){
-        word = word.replace("you", "I");
+      else if (word.equalsIgnoreCase("are")){
+        word = "am";
         mirroredResponse[i] = word;
       }
-      else if (word.equals("my")){
-        word = word.replace("my", "your");
+      else if (word.equalsIgnoreCase("you")){
+        word = "I";
         mirroredResponse[i] = word;
       }
-      else if (word.equals("your")){
-        word = word.replace("your", "my");
+      else if (word.equalsIgnoreCase("my")){
+        word = "your";
+        mirroredResponse[i] = word;
+      }
+      else if (word.equalsIgnoreCase("your")){
+        word = "my";
         mirroredResponse[i] = word;
       }
       else{
@@ -47,71 +62,58 @@ class Conversation {
     return chatbotReply;
   }
 
+  /**
+   * Runs the chatbot and prints a transcript of the conversation.
+   * @param arguments
+   */
   public static void main(String[] arguments) {
     // // You will start the conversation here.
 
-    // //create scanner to get user input and number of rounds
-    //   Scanner chatbot = new Scanner(System.in);
-    //   System.out.println("How many rounds?");
-    //   Integer rounds = chatbot.nextInt();
-    // //start loop with correct number of rounds)
-    //   System.out.println("Hello! Tell me what you're thinking about.");
-    
+    Conversation newConvo = new Conversation();
 
-    //   for (int round_num = 0; round_num < rounds; round_num++) {
-    //     String response = chatbot.next();
-    //     if (rounds <= 10){
-    //       System.out.println(chatbotResponses[round_num]);
-    //     }
-    //     else{
-    //       System.out.println("Yup.");
-    //     }
-    //   }
+    //create scanner to get user input and number of rounds
+      Scanner chatbot = new Scanner(System.in);
+      System.out.println("How many rounds?");
+      Integer rounds = chatbot.nextInt();
+      String[] transcript = new String[(rounds*2)+2];
+    //start loop with correct number of rounds)
+      Scanner chatbot2 = new Scanner(System.in); 
+      System.out.println("Hello! Tell me what you're thinking about.");
+      int transcriptIndex = 0;
+      transcript[transcriptIndex] = "Hello! Tell me what you're thinking about.";
 
-    //   System.out.println("Goodbye");
-
-    public String isMirror(String response){
-      String[] words = response.split(" ");
-      int length = words.length;
-      String[] mirroredResponse = new String[length];
-      for (int i = 0; i < length; i++){
-        String word = words[i];
-        if (word.equals("I")){
-          word = word.replace("I", "you");
-          mirroredResponse[i] = word;
-        }
-        else if (word.equals("me")){
-          word = word.replace("me", "you");
-          mirroredResponse[i] = word;
-        }
-        else if (word.equals("am")){
-          word = word.replace("am", "are");
-          mirroredResponse[i] = word;
-        }
-        else if (word.equals("you")){
-          word = word.replace("you", "I");
-          mirroredResponse[i] = word;
-        }
-        else if (word.equals("my")){
-          word = word.replace("my", "your");
-          mirroredResponse[i] = word;
-        }
-        else if (word.equals("your")){
-          word = word.replace("your", "my");
-          mirroredResponse[i] = word;
+      for (int round_num = 0; round_num < rounds; round_num++) {
+        String response = chatbot2.nextLine();
+        transcriptIndex = transcriptIndex+1;
+        transcript[transcriptIndex] = response;
+        String mirroredWords = newConvo.mirrorWords(response);
+        if (!mirroredWords.equals(response)){
+          System.out.println(mirroredWords);
+          transcriptIndex = transcriptIndex + 1;
+          transcript[transcriptIndex] = mirroredWords;
         }
         else{
-          mirroredResponse[i] = word;
+         if (rounds <= 10){
+            System.out.println(chatbotResponses[round_num]);
+            transcriptIndex = transcriptIndex + 1;
+            transcript[transcriptIndex] = chatbotResponses[round_num];
+          }
+          else{
+            System.out.println("Yup.");
+            transcriptIndex = transcriptIndex + 1;
+            transcript[transcriptIndex] = "Yup.";
+            }
         }
-        
       }
-      String chatbotReply = String.join(" ", mirroredResponse);
-      return chatbotReply;
-    }
-    // String response = "My name is Maddie";
 
-    // response.isMirror(response);
+      System.out.println("Goodbye");
+      transcript[(rounds*2)+1] = "Goodbye";
 
+      System.out.println("");
+      System.out.println("TRANSCRIPT: ");
+      for (int i = 0; i <= transcriptIndex + 1; i++){
+        System.out.println(transcript[i]);
+      }
 
 
 
--- reflection.md
+++ reflection.md
@@ -1,8 +1,17 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I spoke with Ellie Zdancewic, but we spoke on 9/24 after she had already submitted her assignment so I won't be in the list of people she consulted.
 - Which session(s) of TA / office hours did you attend?
-TA hours Tuesday 9/24, 7-9pm
+I attended TA hours on Tuesday 9/24, 7-9pm.
 - What are your initial impressions of Java? 
+Because I don't yet know the syntax well, my initial impressions are that it's a tad confusing. For example, I found writing a for loop to be much more complicated in Java than it is in Python.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Ultimately, while it felt to me like there were a lot of differences between Python and Java because I don't have experience with Java, I also recognize that there are a lot of similarities. The basic structures, like loops, exist in a fairly similar format, even if they are expressed differently. This tells me that the buidling blocks are the same for programming in Java, even if the exact lines needed to make something happen are different.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+My main problem was that I felt like I was starting from scratch in learning a lot of Java syntax. As I've only ever used Python before, I found myself forgetting the differences from time to time and getting a little frustrated. I gave this as my advice on the last assignment as well, but my advice to future students is to take advantage of TA hours. They're very helpful!
+
+
+Sources I consulted: 
+https://www.scaler.com/topics/array-to-string-in-java/
+https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
